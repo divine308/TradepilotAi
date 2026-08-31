d07082bc4c04f4da0b8454663377a0226f3eb0d2
divine308 <okechukwudivine76@gmail.com>
2026-08-31 01:16:22 -0700

    deploy

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -25,7 +25,7 @@ import AppLayout from "../components/AppLayout";
 
 const API_URL = (
   import.meta.env.VITE_BACKEND_URL ||
-  "http://127.0.0.1:8000"
+  "https://tradepilot-h7kw.onrender.com"
 ).replace(/\/$/, "");
 
 
